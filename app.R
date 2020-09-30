75f98dbcc6dacd6ec47067a89063534d764fd216
SandraKla <sandra13897@wp.pl>
2020-09-30 18:12:18 +0200

    Put server.R and ui.R together

--- app.R
+++ app.R
@@ -1,24 +1,33 @@
-# install.packages("remotes")
-# remotes::install_github("LCBC-UiO/ggseg", build_vignettes = TRUE)
-# remotes::install_github("LCBC-UiO/ggseg3d", build_vignettes = TRUE)
-# library(ggseg3d)
-# library(ggsegExtra)
+####################################### Libraries ########################################
 
-library(shiny)
-library(dplyr)
-library(tidyr)
-library(plotly)
-library(ggplot2)
-library(readxl)
 library(colourpicker)
-library(scales)
-library(processx)
-library(reshape2)
 library(cowplot)
-library(ggpubr)
+library(dplyr)
+library(ggplot2)
+#library(ggpubr) 
+
+# Error: package or namespace load failed for ‘ggpubr’ in loadNamespace(j <- i[[1L]], c(lib.loc, .libPaths()), versionCheck = vI[[j]]):
+#   es gibt kein Paket namens ‘car’
+
 library(glmnet)
+library(gridExtra)
 library(gplots)
+library(plotly)
+library(plyr)
+library(processx)
+library(readxl)
+library(reshape2)
+library(randomcoloR)
+library(Rmisc)
+library(scales)
+library(shiny)
 library(shinyWidgets)
+library(shinydashboard)
+library(tidyr)
+library(tidyverse)
+library(XLConnect)
+
+####################################### Scripts ##########################################
 
 source("geom_flat_violin.R")
 source("lm_function_74.R")
@@ -30,13 +39,185 @@ source("choices.R")
 source("lasso_shiny.R")
 source("get_expanded_col.R")
 source("lm_function_ceres.R")
-# source("title_fun.R")
+
 file.source=list.files("ggseg3d//R",pattern="*.R",full.names = TRUE)
 lapply(file.source, source,.GlobalEnv)
 load("desterieux_3d.rda")
+
 sem <- function(x){
   return(sd(x)/sqrt(length(x)))
 }
+
+
+ui <- fluidPage(
+  dashboardPage(
+    
+    dashboardHeader(title="ggseg3d"),
+    dashboardSidebar(
+      sidebarMenu(
+        
+        menuItem("Data",tabName = "Data",icon = icon("file"),
+                 fileInput("data_table","TableInput",accept = c("xlsx","xls")),
+                 prettyRadioButtons("data_type",label = "Data File Type",choices = c("FreeSurfer","CERES","Others"),inline=TRUE)),
+        
+        
+        menuItem("Atlas",tabName = "Atlas",icon=icon("file-alt"),
+                 conditionalPanel(
+                   condition="output.dataFileLoad==true",
+                   fileInput("name_file","Thinkness Names correction",accept = c("xlsx","xls"))
+                 )
+        ),
+        
+        menuItem("Quality Control",tabName = "qc",icon=icon("chart-bar"),expandedName = "qc",
+                 conditionalPanel(
+                   condition="output.dataFileLoad==true",
+                   uiOutput("fil_qc"),
+                   uiOutput("qc_kon"),
+                   actionButton("dp","Distribution Plot")
+                 )
+        ),
+        
+        
+        
+        menuItem("Descriptive Statistics",expandedName = "ds",icon=icon("brain"),
+                 conditionalPanel(
+                   condition="output.dataFileLoad==true && input.data_type=='FreeSurfer'",
+                   uiOutput("fil_ui"),
+                   uiOutput("ds_kon"),
+                   
+                   conditionalPanel(
+                     condition = "input.com==1",
+                     uiOutput("com_cd")
+                   ),
+                   
+                   
+                   prettyRadioButtons(inputId = "select_hemisphere",
+                                      label = "Choose Hemisphere",
+                                      choices = c("left","right","both"),
+                                      selected = "both",inline = TRUE
+                   ),
+                   
+                   checkboxInput("col","Color and Value",FALSE),
+                   
+                   conditionalPanel(condition="input.col==1",
+                                    colourInput("color_obergrenze", "Please select the color of the upper bound", "red"),
+                                    numericInput(inputId = 'wert_obergrenze',
+                                                 label = 'Please choose a value for the upper bound',
+                                                 4.2),
+                                    
+                                    colourInput("color_untergrenze", "Please select the color of the lower bound", "blue"),
+                                    numericInput(inputId = 'wert_untergrenze',
+                                                 label = 'Please choose a value for the lower bound',
+                                                 1.5),
+                                    
+                                    
+                                    
+                                    actionButton("add_mitte", "Add new values and colors"),
+                                    hr(),
+                                    actionButton("remove_mitte","Remove"),
+                                    hr()
+                   ),
+                   checkboxInput("com","composity",value = TRUE),
+                   checkboxInput("check_download","Download",FALSE),
+                   
+                   actionButton("ab","Brain Map"),
+                   conditionalPanel(
+                     condition = "input.check_download==1",
+                     textInput(inputId = "name",
+                               "file name"
+                     ),
+                     
+                     selectInput(inputId = "format",
+                                 "file format",
+                                 choices = c("svg","pdf","png"),
+                                 "pdf"
+                     ),
+                     div(style="height:100px;",
+                         checkboxInput("down_filter","With Filer Label",value = FALSE)
+                         
+                     ),
+                     actionButton("download","Download image"),
+                     hr()
+                   )
+                 )
+        ),
+        menuItem("Linear Regression",expandedName = "ss",icon=icon("chart-line"),
+                 conditionalPanel(
+                   condition="output.dataFileLoad==true",
+                   uiOutput("fil_ss"),
+                   uiOutput("ss_kon"),
+                   actionButton("rp","Regression Plots")
+                 )
+                 
+        ),
+        
+        menuItem("Lasso Regression",expandedName = "ls",icon=icon("chart-line"),
+                 conditionalPanel(
+                   condition="output.dataFileLoad==true",
+                   uiOutput("fil_ls"),
+                   uiOutput("ls_kon"),
+                   actionButton("lp","TableLasso generation")
+                 )
+                 
+        ),
+        
+        tags$button("Restart", 
+                    id="restart", 
+                    type="button", class="btn btn-danger action-button", onclick="history.go(0)"),
+        id="sideM"
+        
+      )
+      
+    ),  
+    
+    
+    dashboardBody(
+      conditionalPanel(
+        condition="output.dataFileLoad==true && input.sidebarItemExpanded=='qc'",
+        uiOutput("qc_tabs")
+      ),
+      
+      conditionalPanel(
+        condition="output.dataFileLoad==true && input.sidebarItemExpanded=='ss'",
+        uiOutput("ss_tabs")
+        # tabsetPanel(
+        #   type="tabs",id="ss_tab",
+        #   tabPanel("Table",DT::dataTableOutput("ss_table")),
+        #   tabPanel("Regression Plots",plotOutput("regression",height ="40000px",width = "1000px"))
+        # ),
+      ),
+      
+      
+      conditionalPanel(
+        condition="output.dataFileLoad==true && input.sidebarItemExpanded=='ds' && input.data_type=='FreeSurfer'",
+        tabsetPanel(
+          type="tabs",id="ds_tab",
+          tabPanel("Table",DT::dataTableOutput("ds_table")),
+          tabPanel("Statistics",DT::dataTableOutput("ds_composity")),
+          tabPanel("3D",plotlyOutput("ggseg3d",height = "700px"))
+        ),
+      ),
+      
+      
+      
+      conditionalPanel(
+        condition="output.dataFileLoad==true && input.sidebarItemExpanded=='ls'",
+        tabsetPanel(
+          type="tabs",id="ls_tab",
+          tabPanel("Table",DT::dataTableOutput("ls_table")),
+          tabPanel("Lasso tabel",DT::dataTableOutput("lasso_table"))
+        ),
+      )
+    )
+  )
+)
+
+
+
+
+
+
+
 
 
 
@@ -58,7 +239,7 @@ server<-function(input, output,session) {
     }
   })
   
-
+  
   
   
   output$dataFileLoad <- reactive({
@@ -210,11 +391,11 @@ server<-function(input, output,session) {
     }else{
       names_explan <- names(OASIS)
     }
-
+    
     return(names_explan)
   })  
   
-
+  
   
   
   
@@ -286,7 +467,7 @@ server<-function(input, output,session) {
   
   
   
-
+  
   
   
   #######################################################
@@ -474,7 +655,7 @@ server<-function(input, output,session) {
     com_cd <<- input$com_way_d
   })
   
-
+  
   
   
   
@@ -492,7 +673,7 @@ server<-function(input, output,session) {
   
   
   
-
+  
   
   
   
@@ -509,7 +690,7 @@ server<-function(input, output,session) {
     ausgewaehlte_daten <- get_choice()
     DT::datatable(ausgewaehlte_daten,class = "display nowrap",options = list(scrollX=TRUE))
   })
-
+  
   
   output$ds_composity <- DT::renderDataTable({
     data <- get_choice()
@@ -522,7 +703,7 @@ server<-function(input, output,session) {
     data_sem <- apply(data, 2, sem)
     
     frame_statistics <- tibble("thickness"=names(data),"mean"=data_mean,"median"=data_median,
-                                   "SD"=data_sd,"SEM"=data_sem)
+                               "SD"=data_sd,"SEM"=data_sem)
     frame_statistics[2:4] <- frame_statistics[2:4]%>%mutate_if(is.numeric,round,2)
     frame_statistics[5] <- frame_statistics[5]%>%mutate_if(is.numeric,round,4)
     
@@ -576,7 +757,7 @@ server<-function(input, output,session) {
     updateNumericInput(session, inputId = "wert_untergrenze", value = min_wert)
     
   }
-)
+  )
   
   
   
@@ -941,7 +1122,7 @@ server<-function(input, output,session) {
                   }else{
                     # tabPanel("Quality Raincloud",plotOutput("quality",height = paste0(((length(input$qc_col)+1)%/%2)*250,"px"),width = "1300px"))
                     tabPanel("Regression Plots",plotOutput("regression",height = "500px",width = "500px"))
-                    }
+                  }
                   
       )
     )
@@ -981,7 +1162,7 @@ server<-function(input, output,session) {
     dat<-get_ls_choice()
     col.length <- length(dat)
     count_lasso<-which(names(dat)==input$lasso_variable)
-
+    
     if(input$data_type=="FreeSurfer"){
       dat <- select(dat,input$lasso_variable,(col.length-147):col.length)
     }else if(input$data_type=="CERES"){
@@ -1005,3 +1186,5 @@ server<-function(input, output,session) {
   
   
 }
+
+shinyApp(ui = ui, server = server)
